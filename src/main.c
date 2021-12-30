32f768fa472573f7fb83bc1292ab85b36b7fdd53
Matiss <matiss.haillouy@gmail.com>
2021-12-30 01:18:59 +0100

    update window

--- src/main.c
+++ src/main.c
@@ -6,22 +6,21 @@
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
 
-
-
-
 #define WINDOW_WIDTH 1366
 #define WINDOW_HEIGHT 768
 #define  LIMIT_FRAME 16
 
 void SDL_ExitWithError(const char *message);
-void SDL_ExitTexture (const char *message, SDL_Window **screen, SDL_Renderer **renderer , SDL_Texture **texture);
+void SDL_ExitTexture (const char *message, SDL_Window **screen, SDL_Renderer **renderer , SDL_Texture **texture,SDL_Texture **texture2);
 void SDL_LimitedFPS(unsigned int limit);
+void SDL_Fenetre2 (const char *message);
 
 int main(int argc, char **argv) {
  SDL_Window *window =NULL;
  SDL_Renderer *renderer =NULL;
 
 
+    int volume=SDL_MIX_MAXVOLUME;
 //les evenements
     SDL_bool program_launched = SDL_TRUE;
     TTF_Init();
@@ -46,7 +45,8 @@ int main(int argc, char **argv) {
     SDL_Surface *fenetre = NULL;
     SDL_Surface *fond =NULL;
     SDL_Texture *texture = NULL;
-        //    fond = IMG_Load("src/fond_principal.png");
+
+    fond = IMG_Load("src/fond_principal.png");
 
     if(fenetre == NULL)
     {
@@ -67,8 +67,8 @@ int main(int argc, char **argv) {
     {
         SDL_ExitTexture;
     }
-    rectangle.x =400;
-    rectangle.y = 600;
+    rectangle.x = 0;
+    rectangle.y = 0;
 
 
     if(SDL_RenderCopy(renderer,texture,NULL,&rectangle)!=0)
@@ -81,34 +81,24 @@ int main(int argc, char **argv) {
     {
         fprintf(stderr, "Erreur d'initialisation TTF : %s\n", TTF_GetError());
     }
+    int w,h;
     TTF_Font * font1;
-    font1 = TTF_OpenFont("JOUER.ttf", 72 );
-    TTF_CloseFont(font1);
-    int iW, iH;
+    font1 = TTF_OpenFont("JOUER.ttf", 111);
+
+    int iW, iH ;
     SDL_Color     couleur  = {255, 255, 255};
-    SDL_Surface * surf     = TTF_RenderText_Blended(font1, "PLAY!", couleur);
+    SDL_Surface * surf = TTF_RenderText_Blended(font1, "PLAY", couleur);
+    rectangle.x = 600;
+    rectangle.y = 300;
+    rectangle.w = 200;
+    rectangle.h = 100;
     SDL_Texture * texttext = SDL_CreateTextureFromSurface(renderer, surf);
     SDL_QueryTexture(texttext, NULL, NULL, &iW, &iH);
-    SDL_RenderCopy(renderer, texttext, NULL, &rectangle);/*
-TTF_Font *fontJOUER;
-fontJOUER= TTF_OpenFont("JOUER.ttf",22);
-SDL_Color fontColor ={0,0,0};
-
-
-//.....................................//
-
-//zone de text
-
-    SDL_Surface *JOUER;
-    SDL_Rect JOUERPosition;
-    printf("le text :%s\n",JOUER);
-    JOUER= TTF_RenderText_Solid(fontJOUER,"PLAY !",fontColor);
-
-    JOUERPosition.x=600;
-    JOUERPosition.y=200;
-    SDL_BlitSurface(fenetre,NULL,JOUER,&JOUERPosition);
-   SDL_FreeSurface(JOUER);
-     int SDL_UpdateWindowSurface(SDL_Window *window);*/
+    if(SDL_RenderCopy(renderer, texttext, NULL, &rectangle))
+    {
+        SDL_ExitTexture;
+    }
+    TTF_CloseFont(font1);
 
 SDL_RenderPresent(renderer);
 while(program_launched)
@@ -121,24 +111,76 @@ while(program_launched)
 
             case SDL_MOUSEBUTTONDOWN:
                     if(event.button.button== SDL_BUTTON_LEFT)
+                        printf("%d/%d\n", event.motion.x,event.motion.y);
+                    if(event.motion.x >=600 && event.motion.x <=800 &&event.motion.y >=300 && event.motion.y <=400 ){
+                        int SDL_UpdateWindowSurface(SDL_Window *window);
+                        SDL_ExitTexture;
+                        // printf("fenetre lancé %s\n",message);
 
-                        // load the MP3 file "music.mp3" to play as music
+                        SDL_Surface *fond2 =NULL;
+                        SDL_Texture *texture2 = NULL;
+
+                        fond2 = IMG_Load("src/image-fond.png");
+
+                        if(fenetre == NULL)
+                        {
+                            SDL_ExitTexture;
+
+                        }
+                        texture2 = SDL_CreateTextureFromSurface(renderer, fenetre);
+                        SDL_FreeSurface(fenetre); //liberation de l'espace
+                        texture2 = SDL_CreateTextureFromSurface(renderer, fond2);
+                        SDL_FreeSurface(fond2); //liberation de l'espace
+
+                        if(texture2 == NULL)
+                        {
+                            SDL_ExitTexture;
+                        }
+                        SDL_Rect rectangle2;
+                        if(SDL_QueryTexture(texture2, NULL , NULL, &rectangle2.w,&rectangle2.h)!=0)
+                        {
+                            SDL_ExitTexture;
+                        }
+                        rectangle2.x = 0;
+                        rectangle2.y = 0;
+                        rectangle2.h = 800;
+                        rectangle2.w = 1366;
 
 
-// start SDL with audio support
+                        if(SDL_RenderCopy(renderer,texture2,NULL,&rectangle2)!=0)
+                        {
+                            SDL_ExitTexture;
+                        }
+                        SDL_RenderPresent(renderer);
+
+                        SDL_bool program_launched = SDL_TRUE;
+                        while(program_launched) {
+                            SDL_Event event;
+                            while (SDL_PollEvent(&event))
+                                switch (event.type) {
+                                    {
+                                        case SDL_MOUSEBUTTONDOWN:
+                                            if(event.button.button== SDL_BUTTON_RIGHT)
+                                                printf("clic droit effectué\n");
+                                    }
+                                    case SDL_QUIT:
+                                        program_launched = SDL_FALSE;
+                                        break;
+                                    default:
+                                        break;}
+                        }
+                        //SDL_Fenetre2;
+                        continue;
+                    }
+
                         if(SDL_Init(SDL_INIT_AUDIO)==-1) {
                             printf("SDL_Init: %s\n", SDL_GetError());
                             exit(1);
                         }
-// open 44.1KHz, signed 16bit, system byte order,
-//      stereo audio, using 1024 byte chunks
                 if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024)==-1) {
                     printf("Mix_OpenAudio: %s\n", Mix_GetError());
                     exit(2);
                 }
-
-
-// load support for the OGG and MOD sample/music formats
                 int flags=MIX_INIT_OGG|MIX_INIT_MOD;
                 int initted=Mix_Init(flags);
                 if(initted&flags != flags) {
@@ -149,16 +191,11 @@ while(program_launched)
                 Mix_Music *music;
                 music=Mix_LoadMUS("src/music.mp3");
                 if(!music) {
-                    printf("Mix_LoadMUS(\"goku.mp3\"): %s\n", Mix_GetError());
+                    printf("Mix_LoadMUS(\"music.mp3\"): %s\n", Mix_GetError());
                     // this might be a critical error...
                 }
-
-
-                // play music forever
-                // Mix_Music *music; // I assume this has been loaded already
                 if(Mix_PlayMusic(music, -1)==-1) {
                     printf("Mix_PlayMusic: %s\n", Mix_GetError());
-                    // well, there's no music, but most games don't break without music...
                 }
                 // resume music playback
                 Mix_ResumeMusic();
@@ -179,16 +216,22 @@ while(program_launched)
             case SDL_KEYDOWN:
                 switch (event.key.keysym.scancode)
                 {
-                    case SDL_SCANCODE_DOWN:
-                        printf("vous avez appuye sur la fleche du bas \n");
-                        // set the music volume to 1/2 maximum, and then check it
-                        printf("volume was    : %d\n", Mix_VolumeMusic(MIX_MAX_VOLUME/2));
-                        printf("volume is now : %d\n", Mix_VolumeMusic(-1));
-                        continue;
                     case SDL_SCANCODE_UP:
-// set the music volume to 1/2 maximum, and then check it
-                        printf("volume was    : %d\n", Mix_VolumeMusic(MIX_MAX_VOLUME*2));
-                        printf("volume is now : %d\n", Mix_VolumeMusic(+1));
+                        // set the music volume to 1/2 maximum, and then check it
+                        printf("volume est a    : %d\n", Mix_VolumeMusic(MIX_MAX_VOLUME*2));
+                        printf("volume passe a  : %d\n", Mix_VolumeMusic(+1));
+                        volume<<=1;
+                        Mix_VolumeMusic(volume);
+
+                        continue;
+                    case SDL_SCANCODE_DOWN:
+                            printf("vous avez appuye sur la fleche du bas \n");
+                            // set the music volume to 1/2 maximum, and then check it
+                            printf("volume est a   : %d\n", Mix_VolumeMusic(MIX_MAX_VOLUME / 2));
+                            printf("volume passe a: %d\n", Mix_VolumeMusic(-1));
+                        volume>>=1;
+                        Mix_VolumeMusic(volume);
+                        continue;
 
                     case SDL_SCANCODE_B:
 
@@ -209,10 +252,11 @@ while(program_launched)
         }
     }
 }
+
 //.......................................//
 //fps
 unsigned  int frame_limited =0;
-frame_limited = SDL_GetTicks()+LIMIT_FRAME; //debut
+    frame_limited = SDL_GetTicks()+LIMIT_FRAME; //debut
     SDL_LimitedFPS(frame_limited); // pour controle la limite
     frame_limited = SDL_GetTicks()+LIMIT_FRAME;// fin pour s assurer de la syncronisation
 
@@ -232,11 +276,12 @@ void SDL_LimitedFPS(unsigned int limit)
         else
         SDL_Delay(limit - ticks);
 }
-void SDL_ExitTexture (const char *message, SDL_Window **screen, SDL_Renderer **renderer , SDL_Texture **texture)
+void SDL_ExitTexture (const char *message, SDL_Window **screen, SDL_Renderer **renderer , SDL_Texture **texture,SDL_Texture **texture2)
 {
     SDL_DestroyRenderer(*renderer);
     SDL_DestroyWindow(*screen);
     SDL_DestroyTexture(*texture);
+    SDL_DestroyTexture(*texture2);
     SDL_ExitWithError(message);
 }
 
@@ -246,4 +291,17 @@ void SDL_ExitWithError(const char *message)
     SDL_Quit();
     exit(EXIT_FAILURE);
 }
+
+void SDL_Fenetre2 (const char *message)
+{
+    SDL_Window *window =NULL;
+    SDL_Renderer *renderer =NULL;
+
+}
+
+
+
+
+
+
 //gcc src/main.c -o bin/prog -I include -L lib  -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf
